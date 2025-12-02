8012753ce5d11c9ac06b30636db61ee06b6fc7f5
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-12-03 02:15:41 +0800

    feat: add depth and fixed formatting

--- utils/node.hpp
+++ utils/node.hpp
@@ -9,6 +9,7 @@ struct Node
     Node *right;
     bool is_leaf;
     int label;
+    int depth;
 
-    Node() : feature_index(-1), threshold(0.0), left(nullptr), right(nullptr), is_leaf(false), label(-1) {}
-}
+    Node() : feature_index(-1), threshold(0.0), left(nullptr), right(nullptr), is_leaf(false), label(-1), depth(0) {}
+};

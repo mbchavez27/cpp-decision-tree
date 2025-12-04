56c203d065454957008e10278f3b51e8a35e4e54
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-12-04 13:27:30 +0900

    feat: finished decision tree skeleton code

---
+++ utils/decision_tree.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "node.hpp"
+#include <vector>
+using namespace std;
+
+class DecisionTree
+{
+public:
+    DecisionTree(int max_depth = 5);
+    void fit(const vector<double> &X, const vector<double> &y);
+    int predict(const vector<double> &X);
+
+private:
+    Node *root;
+    int max_depth;
+    Node *build_tree(const vector<double> &X, const vector<int> &y, int depth);
+    int best_split(const vector<double> &X, const vector<int> &y, double &threshold);
+};
